add93a80b7021b0f20b07db6b182f1b0f9e2cf0e
HippolyteGuigon <guigonhippolyte@yahoo.fr>
2026-07-12 12:06:00 +0200

    added loading data function

--- functional_supervised_classification/data_loading.py
+++ functional_supervised_classification/data_loading.py
@@ -1,4 +1,18 @@
 from aeon.datasets import load_classification   
 
-X_train, y_train = load_classification("ECG200", split="train")
-X_test, y_test = load_classification("ECG200", split="test")
+def load_ecg200():
+    """
+    The goal of this function is to load the ECG200 dataset from the aeon library. 
+    It returns the training and testing data along with their corresponding labels.
+    
+    Returns:
+    X_train: np.ndarray: Training data features
+    y_train: np.ndarray: Training data labels
+    X_test: np.ndarray: Testing data features
+    y_test: np.ndarray: Testing data labels
+    """
+
+    X_train, y_train = load_classification("ECG200", split="train")
+    X_test, y_test = load_classification("ECG200", split="test")
+
+    return X_train, y_train, X_test, y_test
